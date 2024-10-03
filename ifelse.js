095622905ed7d79f7dda9530ca8d4c28262ae447
Sumanth-9515 <myakalasumanthreddy@gmail.com>
2024-10-03 21:47:52 +0530

    Add files via upload

---
+++ ifelse.js
@@ -0,0 +1,75 @@
+// task on 30
+//1
+for(a=1;a<=10;a++){   //using for loop
+    console.log(a);
+    
+}
+
+a=1
+while(a<=10){         //using while loop
+    console.log(a);
+    a++   
+}
+// 2
+for(a=1;a<=10;a++){
+    if(a%2==0){
+        console.log(a + "is even number"); 
+    }
+}
+//2
+b=1
+while(b<10){
+    if(b%2==0){
+        console.log(b + "is even number");
+    }
+    b++
+}
+//3
+
+for(a=1;a<=10;a++){
+    if(a%2==1){
+        console.log(a + "is odd number"); 
+    }
+}
+//3
+b=1
+while(b<10){
+    if(b%2==1){
+        console.log(b + "is odd number");
+    }
+    b++
+}
+//4
+i=35
+j=45
+if(i<=j){
+    console.log("grade D");
+}
+
+
+// var a = 1;
+// if (a == 2) {
+//     console.log("sumanth");
+// }
+// else {
+//     console.log("akash");
+// }
+
+// var highest_sore_IPL = 287;
+
+// if (highest_sore_IPL == 263){
+//     console.log("RCB");
+// }else if(highest_sore_IPL==225){
+//     console.log("MI");  
+// }else if(highest_sore_IPL==287){
+//     console.log("SRH");
+// }else if(highest_sore_IPL==205){
+//     console.log("CSK");
+// }else{
+//     console.log("non of the above");
+// }
+
+
+// for(b=1;b<=10;b++){
+//     console.log(b);
+    
